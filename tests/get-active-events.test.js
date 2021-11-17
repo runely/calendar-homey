e9db26ce4ce9a47a4c5bf23edd8af1bec1af42a3
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-11-17 21:43:47 +0100

    Removed unneccessary tests

--- tests/get-active-events.test.js
+++ tests/get-active-events.test.js
@@ -60,39 +60,11 @@ describe('getActiveEvents returns', () => {
 })
 
 describe('getActiveEvents returns an array', () => {
-  test('With 2 or 3 \'OnceAWeek\' events (depending on date and time)', () => {
-    expect(onceAWeekEvents.length === 2 || onceAWeekEvents.length === 3).toBeTruthy()
-  })
-
-  test('With 3 or 4 \'AlwaysOngoing\' events', () => {
-    expect(alwaysOngoingEvents.length === 3 || alwaysOngoingEvents.length === 4).toBeTruthy()
-  })
-
   test('Where all \'OnceAWeek\' events has a unique uid', () => {
     expect(onceAWeekEvents.filter(event => event.uid === `hidden_${event.start.toDate().toISOString().slice(0, 10)}`).length).toBe(onceAWeekEvents.length)
   })
 
   test('Where all \'AlwaysOngoing\' events has a unique uid', () => {
     expect(alwaysOngoingEvents.filter(event => event.uid === `hidden2_${event.start.toDate().toISOString().slice(0, 10)}`).length).toBe(alwaysOngoingEvents.length)
-  })
-})
-
-describe('Events has correct time', () => {
-  test('\'OnceAWeek\' is 17:30:00 in UTC', () => {
-    const event = onceAWeekEvents[0]
-    const time = event.start.utc().toISOString().split('T')[1].split('.')[0].split(':')
-    console.log('OnceAWeek:', time)
-    expect(time[0] === '16' || time[0] === '17').toBeTruthy()
-    expect(time[1]).toBe('30')
-    expect(time[2]).toBe('00')
-  })
-
-  test('\'AlwaysOngoing\' is 03:00:00 in UTC', () => {
-    const event = alwaysOngoingEvents[0]
-    const time = event.start.utc().toISOString().split('T')[1].split('.')[0].split(':')
-    console.log('AlwaysOnGoing:', time)
-    expect(time[0] === '01' || time[0] === '02' || time[0] === '03').toBeTruthy()
-    expect(time[1]).toBe('00')
-    expect(time[2]).toBe('00')
   })
 })
