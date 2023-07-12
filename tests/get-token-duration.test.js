16c95681b8675745df609b9b568c87d9551f80b4
Rune Moskvil Lyngås <runely85@gmail.com>
2023-07-12 13:00:16 +0200

    Added test to check humanize-duration on long events

--- tests/get-token-duration.test.js
+++ tests/get-token-duration.test.js
@@ -24,24 +24,42 @@ const event = {
   summary: 'startNow'
 }
 
-test('Returns an object', () => {
-  const result = getTokenDuration(app, event)
-  expect(result).toBeTruthy()
-})
+const eventLong = {
+  start: moment(),
+  datetype: 'date-time',
+  end: moment().add(15789, 'minutes'),
+  uid: 'F7177A32-DBD4-46A9-85C7-669749EA8841',
+  description: 'Desc',
+  location: '',
+  summary: 'startNow'
+}
 
-test(' ...with 2 properties: "duration" and "durationMinutes"', () => {
-  const result = getTokenDuration(app, event)
-  expect(Object.getOwnPropertyNames(result).length).toBe(2)
-})
+describe('getTokenDuration', () => {
+  test('Returns an object', () => {
+    const result = getTokenDuration(app, event)
+    expect(result).toBeTruthy()
+  })
 
-test(' ...where "duration" is of type "string" and is "1 hour"', () => {
-  const result = getTokenDuration(app, event)
-  expect(typeof result.duration).toBe('string')
-  expect(result.duration).toBe('1 hour')
-})
+  test('Returns an object with 2 properties: "duration" and "durationMinutes"', () => {
+    const result = getTokenDuration(app, event)
+    expect(Object.getOwnPropertyNames(result).length).toBe(2)
+  })
 
-test(' ...where "durationMinutes" is of type "number" and is 60', () => {
-  const result = getTokenDuration(app, event)
-  expect(typeof result.durationMinutes).toBe('number')
-  expect(result.durationMinutes).toBe(60)
+  test('Returns an object where "duration" is of type "string" and is "1 hour"', () => {
+    const result = getTokenDuration(app, event)
+    expect(typeof result.duration).toBe('string')
+    expect(result.duration).toBe('1 hour')
+  })
+
+  test('Returns an object where "duration" is of type "string" and is "1 week, 3 days and 23 hours"', () => {
+    const result = getTokenDuration(app, eventLong)
+    expect(typeof result.duration).toBe('string')
+    expect(result.duration).toBe('1 week, 3 days and 23 hours')
+  })
+
+  test('Returns an object where "durationMinutes" is of type "number" and is 60', () => {
+    const result = getTokenDuration(app, event)
+    expect(typeof result.durationMinutes).toBe('number')
+    expect(result.durationMinutes).toBe(60)
+  })
 })
