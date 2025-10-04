b7847d288fd33cdbaa20d978eabd579806faec5d
RoyK <rkokk@protonmail.com>
2025-10-04 09:01:44 +0200

    refactor(widget): getEvents function

--- widgets/calendar-short/api.js
+++ widgets/calendar-short/api.js
@@ -5,24 +5,50 @@ const getDayKey = (datetime) => {
   return datetime.clone().startOf('day');
 }
 
+/**
+ * Creates a copy of the calendar object without the 'events' property.
+ * @param {Object} calendar - The calendar object to process.
+ * @returns {Object} - The calendar object without the 'events' property.
+ */
+const createCalendarWithoutEvents = (calendar) => {
+  const { events: _, ...calendarWithoutEvents } = calendar;
+  return calendarWithoutEvents;
+}
+
+/**
+ * Adds the calendar reference to each event in the calendar.
+ * @param {Object} calendar - The calendar object containing events.
+ * @param {Object} calendarWithoutEvents - The calendar object without the 'events' property.
+ * @returns {Array} - An array of events with the calendar reference added.
+ */
+const addCalendarReferenceToEvents = (calendar, calendarWithoutEvents) => {
+  return calendar.events.map(event => ({
+    ...event,
+    calendar: calendarWithoutEvents
+  }));
+}
+
+/**
+ * Lists all relevant events from the calendars managed by the homey app.
+ * @param {Object} homey - The homey app object containing calendar data.
+ * @returns {Array} - An array of all relevant events with calendar references.
+ */
+const listRelevantEvents = (homey) => {
+  return homey.app.variableMgmt.calendars.reduce(
+    (allEvents, calendar) => {
+      const calendarWithoutEvents = createCalendarWithoutEvents(calendar);
+      const eventsWithCalendar = addCalendarReferenceToEvents(calendar, calendarWithoutEvents);
+      return allEvents.concat(eventsWithCalendar);
+    },
+    []
+  );
+}
+
 module.exports = {
 
   async getCalendarList({ homey, query }) {
     const timeFormat = homey.app.variableMgmt.dateTimeFormat.time;
-    let events = homey.app.variableMgmt.calendars.reduce(
-      (allEvents, calendar) => {
-        // Create a copy of the calendar object without the 'events' property
-        const { events: _, ...calendarWithoutEvents } = calendar;
-
-        // For each event in the calendar, add the calendar reference
-        const eventsWithCalendar = calendar.events.map(event => ({
-          ...event,
-          calendar: calendarWithoutEvents // Attach the calendar reference
-        }));
-        return allEvents.concat(eventsWithCalendar);
-      },
-      []
-    );
+    let events = listRelevantEvents(homey);
 
     // Ensure max X row is shown
     events = sortEvents(events);
@@ -49,7 +75,7 @@ module.exports = {
       events.forEach((event, index) => {
         // Create dayCell for the first event of the day
         const dayInfo = index === 0
-          ? `<span class="homey-text-small homey-text-align-center">${day.format('ddd')}<br />${day.format('MMM D')}</span>`
+          ? `<span class=\"homey-text-small homey-text-align-center\">${day.format('ddd')}<br />${day.format('MMM D')}</span>`
           : null;
 
         // Create period string
@@ -64,8 +90,8 @@ module.exports = {
         period = period + after;
 
         // Create summaryCell and calendarCell
-        const summaryInfo = `<span class="homey-text-small">${event.summary}</span><br /><span style="color: var(--homey-color-highlight);" class="homey-text-small-light">${period}</span>`;
-        const calendarInfo = `<span style="color: ${event.calendar.color};" class="widget-calendar-cell homey-text-small"><div style="--homey-icon-color: ${event.calendar.color}" class="homey-custom-icon-calendar"></div>${event.calendar.name}</span>`;
+        const summaryInfo = `<span class=\"homey-text-small\">${event.summary}</span><br /><span style=\"color: var(--homey-color-highlight);\" class=\"homey-text-small-light\">${period}</span>`;
+        const calendarInfo = `<span style=\"color: ${event.calendar.color};\" class=\"widget-calendar-cell homey-text-small\"><div style=\"--homey-icon-color: ${event.calendar.color}\" class=\"homey-custom-icon-calendar\"></div>${event.calendar.name}</span>`;
 
         // Push serialized event data
         serializedEvents.push({
