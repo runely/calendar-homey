8c34ba64db388716c77de71b87fcf3f63a843e8a
RoyK <rkokk@protonmail.com>
2025-10-04 09:09:24 +0200

    fix(widget): api.js format errors

--- widgets/calendar-short/api.js
+++ widgets/calendar-short/api.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 const sortEvents = require('./../../lib/sort-events')
 
 /**
@@ -7,7 +7,7 @@ const sortEvents = require('./../../lib/sort-events')
  * @returns {Object} - The day key for the given datetime.
  */
 const getDayKey = (datetime) => {
-  return datetime.clone().startOf('day');
+  return datetime.clone().startOf('day')
 }
 
 /**
@@ -16,8 +16,8 @@ const getDayKey = (datetime) => {
  * @returns {Object} - The calendar object without the 'events' property.
  */
 const createCalendarWithoutEvents = (calendar) => {
-  const { events: _, ...calendarWithoutEvents } = calendar;
-  return calendarWithoutEvents;
+  const { events: _, ...calendarWithoutEvents } = calendar
+  return calendarWithoutEvents
 }
 
 /**
@@ -30,7 +30,7 @@ const addCalendarReferenceToEvents = (calendar, calendarWithoutEvents) => {
   return calendar.events.map(event => ({
     ...event,
     calendar: calendarWithoutEvents
-  }));
+  }))
 }
 
 /**
@@ -41,12 +41,12 @@ const addCalendarReferenceToEvents = (calendar, calendarWithoutEvents) => {
 const listRelevantEvents = (homey) => {
   return homey.app.variableMgmt.calendars.reduce(
     (allEvents, calendar) => {
-      const calendarWithoutEvents = createCalendarWithoutEvents(calendar);
-      const eventsWithCalendar = addCalendarReferenceToEvents(calendar, calendarWithoutEvents);
-      return allEvents.concat(eventsWithCalendar);
+      const calendarWithoutEvents = createCalendarWithoutEvents(calendar)
+      const eventsWithCalendar = addCalendarReferenceToEvents(calendar, calendarWithoutEvents)
+      return allEvents.concat(eventsWithCalendar)
     },
     []
-  );
+  )
 }
 
 module.exports = {
@@ -57,63 +57,63 @@ module.exports = {
    * @param {Object} query - The query parameters for filtering events.
    * @returns {Array} - An array of serialized events for display.
    */
-  async getCalendarList({ homey, query }) {
-    const timeFormat = homey.app.variableMgmt.dateTimeFormat.time;
-    let events = listRelevantEvents(homey);
+  async getCalendarList ({ homey, query }) {
+    const timeFormat = homey.app.variableMgmt.dateTimeFormat.time
+    let events = listRelevantEvents(homey)
 
     // Ensure max X row is shown
-    events = sortEvents(events);
-    events = events.slice(0, query.max);
+    events = sortEvents(events)
+    events = events.slice(0, query.max)
 
     // Group events by day and serialize the data
-    const serializedEvents = [];
+    const serializedEvents = []
     const eventsByDay = events.reduce((map, event) => {
-      const dayKey = getDayKey(event.start).format('YYYY-MM-DD');
+      const dayKey = getDayKey(event.start).format('YYYY-MM-DD')
       if (!map.has(dayKey)) {
         map.set(dayKey, {
           day: dayKey,
-          events: [],
-        });
+          events: []
+        })
       }
-      map.get(dayKey).events.push(event);
-      return map;
-    }, new Map());
+      map.get(dayKey).events.push(event)
+      return map
+    }, new Map())
 
     eventsByDay.forEach((dayEvent, _) => {
-      const day = dayEvent.day;
-      const events = dayEvent.events;
+      const day = dayEvent.day
+      const events = dayEvent.events
 
       events.forEach((event, index) => {
         // Create dayCell for the first event of the day
         const dayInfo = index === 0
-          ? `<span class=\"homey-text-small homey-text-align-center\">${day.format('ddd')}<br />${day.format('MMM D')}</span>`
-          : null;
+          ? `<span class='homey-text-small homey-text-align-center'>${day.format('ddd')}<br />${day.format('MMM D')}</span>`
+          : null
 
         // Create period string
-        let after =  ` - ${event.end.format(timeFormat)}`;
-        let period = event.fullDayEvent ? homey.__("widget.allDay") : event.start.format(timeFormat);
+        let after = ` - ${event.end.format(timeFormat)}`
+        let period = event.fullDayEvent ? homey.__('widget.allDay') : event.start.format(timeFormat)
 
         if (!event.start.isSame(event.end, 'day')) {
-          const endFormat = event.fullDayEvent ? 'D MMMM' : `D MMMM ${timeFormat}`;
-          after = event.fullDayEvent ? `, ${homey.__("widget.until")} ${event.end.format(endFormat)}` : ` - ${event.end.format(endFormat)}`;
+          const endFormat = event.fullDayEvent ? 'D MMMM' : `D MMMM ${timeFormat}`
+          after = event.fullDayEvent ? `, ${homey.__('widget.until')} ${event.end.format(endFormat)}` : ` - ${event.end.format(endFormat)}`
         }
 
-        period = period + after;
+        period = period + after
 
         // Create summaryCell and calendarCell
-        const summaryInfo = `<span class=\"homey-text-small\">${event.summary}</span><br /><span style=\"color: var(--homey-color-highlight);\" class=\"homey-text-small-light\">${period}</span>`;
-        const calendarInfo = `<span style=\"color: ${event.calendar.color};\" class=\"widget-calendar-cell homey-text-small\"><div style=\"--homey-icon-color: ${event.calendar.color}\" class=\"homey-custom-icon-calendar\"></div>${event.calendar.name}</span>`;
+        const summaryInfo = `<span class='homey-text-small'>${event.summary}</span><br /><span style='color: var(--homey-color-highlight);' class='homey-text-small-light'>${period}</span>`
+        const calendarInfo = `<span style='color: ${event.calendar.color};' class='widget-calendar-cell homey-text-small'><div style='--homey-icon-color: ${event.calendar.color}' class='homey-custom-icon-calendar'></div>${event.calendar.name}</span>`
 
         // Push serialized event data
         serializedEvents.push({
           dayInfo,
           summaryInfo,
-          calendarInfo,
-        });
-      });
-    });
+          calendarInfo
+        })
+      })
+    })
 
-    return serializedEvents;
+    return serializedEvents
   },
 
   /**
@@ -122,8 +122,8 @@ module.exports = {
    * @param {Object} params - The parameters for filtering events.
    * @returns {Object} - An empty object as a placeholder.
    */
-  async getCalendarEvents({ homey, params }) {
+  async getCalendarEvents ({ homey, params }) {
     // access the post body and perform some action on it.
     return {}
   }
-};
+}
